bf0ab548458b00eed20aeedf091cb295caba0d86
berk <sbgonenc@gmail.com>
2023-04-19 16:34:34 +0100

    created PhyloTree class

--- phylogenetic_tree.py
+++ phylogenetic_tree.py
@@ -37,3 +37,101 @@ the left and the right side [49]
 staircaseness_2 Mean ratio of the minimal number of leaves on a side over the maximal number of leaves
 on a side, for each internal node [49, 50]
 """
+
+from ete3 import Tree
+from ete3.parser.newick import NewickError
+
+
+class PhyloTree:
+    def __init__(self, tree_path, tree_format=5, tree=None):
+        """
+        :param tree: tree file
+        :param tree_format: values may be needed by ete3 package
+        """
+        self.tree_path = tree_path
+        self.tree_format = tree_format
+        self.tree = tree if tree is not None else self.read_tree(self.tree_path)
+
+
+        self.max_H = None
+        self.min_H = None
+        self.a_BL_mean = None
+        self.a_BL_median = None
+        self.a_BL_var = None
+
+        self.tree_stats= None
+
+        ## Collect all distances from root to leaf
+        self._leaf_distances = None
+
+
+    def update_tree_stats(self):
+        self.tree_stats = {
+            "max_H": self.max_H,
+            "min_H": self.min_H,
+            "a_BL_mean": self.a_BL_mean,
+            "a_BL_median": self.a_BL_median
+        }
+    def get_tree_stats(self):
+        self._leaf_distances = [self.tree.get_distance(leaf) for leaf in self.tree.get_leaves()]
+        self._get_min_distance_from_root()
+        self._get_max_distance_from_root()
+        self._get_bl_mean()
+        self._get_bl_median()
+        self.update_tree_stats()
+        return self.tree_stats
+
+    def _get_min_distance_from_root(self):
+        self.min_H = self.tree.get_closest_leaf()[1]
+        #self.min_H = min(self._leaf_distances)
+    def _get_max_distance_from_root(self):
+        self.max_H = self.tree.get_farthest_leaf()[1]
+        #self.max_H = max(self._leaf_distances)
+
+    def _get_bl_mean(self):
+        self.a_BL_mean = sum(self._leaf_distances) / len(self._leaf_distances)
+
+    def _get_bl_median(self):
+        self.a_BL_median = sorted(self._leaf_distances)[len(self._leaf_distances) // 2]
+
+
+
+    @staticmethod
+    def read_tree(newick_tree): ###Taken from PhyloDeep model code
+        """ Tries all nwk formats and returns an ete3 Tree
+
+        :param newick_tree: str, a tree in newick format
+        :return: ete3.Tree
+        """
+        tree = None
+        for f in (3, 2, 5, 0, 1, 4, 6, 7, 8, 9):
+            try:
+                tree = Tree(newick_tree, format=f)
+                break
+            except NewickError as e:
+                print(e)
+                continue
+        if not tree:
+            raise ValueError('Could not read the tree {}. Is it a valid newick?'.format(newick_tree))
+        return tree
+
+    def read_tree_file(self): ### Taken from PhyloDeep model code
+        with open(self.tree_path, 'r') as f:
+            nwk = f.read().replace('\n', '').split(';')
+            if nwk[-1] == '':
+                nwk = nwk[:-1]
+        if not nwk:
+            raise ValueError('Could not find any trees (in newick format) in the file {}.'.format(self.tree_path))
+        if len(nwk) > 1:
+            raise ValueError('There are more than 1 tree in the file {}. Now, we accept only one tree per inference.'.format(self.tree_path))
+        return self.read_tree(nwk[0] + ';')
+
+
+
+#instr = "((Sample1:0.0,Sample0:0.09333496093749999):0.1,(Sample2:0.1,Sample3:0.0):0.1):1.0;"
+#phyltree = PhyloTree(
+#    tree_path="MAPLE_FWsim_out_tree.tree"
+#)
+#phyltree.get_tree_stats()
+#print(phyltree.tree_stats)
+#phyltree.tree.render("tryandcry.png")
