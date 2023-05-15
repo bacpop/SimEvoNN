80217ae3db9bc4592ef72bf7460a495eaa8afcd4
berk <sbgonenc@gmail.com>
2023-05-15 15:43:25 +0100

    included MAPLE files within the project

---
+++ MAPLE/__init__.py
@@ -0,0 +1,4 @@
+## Requires pypy env
+import os
+createMapleFile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "createMapleFile.py")
+run_Maple = os.path.join(os.path.dirname(os.path.abspath(__file__)), "run_Maple.py")
